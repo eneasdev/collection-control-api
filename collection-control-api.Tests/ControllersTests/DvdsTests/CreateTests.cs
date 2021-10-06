99ac6362aa1710cb94c76cddca24f40143f71e63
Lucas Martins <LucasMartins_Eu@hotmail.com>
2021-10-06 20:22:39 -0300

    Dvd Create method has been created before but for some reason i forgot to make the tests. Done

---
+++ collection-control-api.Tests/ControllersTests/DvdsTests/CreateTests.cs
@@ -0,0 +1,46 @@
+﻿using collection_control_api.Controllers;
+using collection_control_api.Entities;
+using collection_control_api.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace collection_control_api.Tests.ControllersTests.DvdsTests
+{
+    public class CreateTests
+    {
+        [Fact]
+        public void ValidObjectIsPassed_ExecuteCreate_CreateShouldReturnAOkResult()
+        {
+            // Arrange
+            var dvdServiceMock = new Mock<IDvdService>();
+            var dvdController = new DvdsController(dvdServiceMock.Object);
+
+            var newDvd = new Dvd("Rei leão", "Simba");
+
+            // Act
+            var resultado = dvdController.Create(newDvd) as OkResult;
+
+            // Assert
+            Assert.True(resultado.StatusCode == 200);
+        }
+
+        [Fact]
+        public void NullIsPassed_ExecuteCreate_CreateShouldReturnBadRequestResult()
+        {
+            // Arrange
+            var dvdServiceMock = new Mock<IDvdService>();
+            var dvdController = new DvdsController(dvdServiceMock.Object);
+
+            Dvd newDvd = null;
+            // Act
+            var resultado = dvdController.Create(newDvd) as BadRequestResult;
+
+            // Assert
+            Assert.True(resultado.StatusCode == 400);
+        }
+    }
+}
