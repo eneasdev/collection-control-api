da5ab853daba72b8171498586a0d68bd64239c14
eneasdev <lucasmartins_eu@hotmail.com>
2021-11-10 10:50:26 -0300

    Clients tests are finished

---
+++ collection-control-api.Tests/ControllersTests/ClientTests/GetAllTests.cs
@@ -0,0 +1,33 @@
+﻿using collection_control_api.Controllers;
+using collection_control_api.Entities;
+using collection_control_api.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace collection_control_api.Tests.ControllersTests.ClientTests
+{
+    public class GetAllTests
+    {
+        [Fact]
+        public void ExecuteGetAll_GetByIdShouldReturnOkResult()
+        {
+            // Arrange
+            var clientRepository = new Mock<IClientRepository>();
+            var clientController = new ClientsController(clientRepository.Object);
+
+            List<Client> listClients = new List<Client>();
+
+            clientRepository.Setup(c => c.GetAll()).Returns(listClients);
+
+            // Act
+            var resultado = clientController.GetAll() as OkObjectResult;
+
+            // Assert
+            Assert.True(resultado.StatusCode == 200);
+        }
+    }
+}
--- collection-control-api.Tests/ControllersTests/ClientTests/GetByIdTests.cs
+++ collection-control-api.Tests/ControllersTests/ClientTests/GetByIdTests.cs
@@ -32,5 +32,37 @@ namespace collection_control_api.Tests.ControllersTests.ClientTests
             // Assert
             Assert.True(resultado.StatusCode == 200);
         }
+
+        [Fact]
+        public void InvalidIdIsPassed_ExecuteGetById_GetByIdShouldReturnABadRequestResult()
+        {
+            // Arrange
+            var clientRepository = new Mock<IClientRepository>();
+            var clientController = new ClientsController(clientRepository.Object);
+
+            // Act
+            var resultado = clientController.GetById(-1) as BadRequestResult;
+
+            // Assert
+            Assert.True(resultado.StatusCode == 400);
+        }
+
+        [Fact]
+        public void ValidIdIsPassed_ExecuteGetById_GetByIdShouldReturnNotFoundResult()
+        {
+            // Arrange
+            var clientRepository = new Mock<IClientRepository>();
+            var clientController = new ClientsController(clientRepository.Object);
+
+            Client client = null;
+
+            clientRepository.Setup(c => c.GetById(1)).Returns(client);
+
+            // Act
+            var resultado = clientController.GetById(1) as NotFoundResult;
+
+            // Assert
+            Assert.True(resultado.StatusCode == 404);
+        }
     }
 }
